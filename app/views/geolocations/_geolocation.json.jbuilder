d46a618df0de4276b00c33d1e1bb2c36865daac1
PerrySawatzky <sawatzky.perry@gmail.com>
2021-06-28 17:05:44 -0400

    functioning js google map API

---
+++ app/views/geolocations/_geolocation.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! geolocation, :id, :created_at, :updated_at
+json.url geolocation_url(geolocation, format: :json)
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_18_193041) do
+ActiveRecord::Schema.define(version: 2021_06_28_165457) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb3", force: :cascade do |t|
     t.string "name", null: false
@@ -132,6 +132,11 @@ ActiveRecord::Schema.define(version: 2021_06_18_193041) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "title"
+  end
+
+  create_table "geolocations", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb3", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "leads", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb3", force: :cascade do |t|
