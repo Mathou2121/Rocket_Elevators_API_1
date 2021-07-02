fa8d263bacb69f7d6878c02de18fed12ef511227
Mathieu Dion <mathieu.dion98@gmail.com>
2021-07-02 09:27:25 -0400

    saving dropbox changes

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,4 +1,21 @@
+require 'dropbox_api'
+
 class Customer < ApplicationRecord
     has_many :buildings
     has_one :user
+
+    after_commit :dropbox_api, on: :create
+
+
+    def dropbox_api 
+        Lead.all.each do |lead|
+            if lead.email == self.company_contact_email || lead.email == self.technical_manager_email_for_service
+                binary_file = lead.attached_file
+                client = DropboxApi::Client.new("K4B7dPp-39kAAAAAAAAAAXZQaoSZFGn1uXjAOokUl39uftGvHtrtD2tmX7fnN5p1")
+                client.add_folder
+                client.upload '/', binary_file
+                
+            end       
+        end
+    end
 end
