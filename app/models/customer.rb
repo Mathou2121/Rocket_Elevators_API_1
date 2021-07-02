11d5731d0ea2a47bec5b57f493dc9e0400598042
Mathieu Dion <mathieu.dion98@gmail.com>
2021-07-02 14:12:11 -0400

    aposid

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -11,7 +11,7 @@ class Customer < ApplicationRecord
         Lead.all.each do |lead|
             if lead.email == self.company_contact_email || lead.email == self.technical_manager_email_for_service
                 binary_file = lead.file.download
-                client = DropboxApi::Client.new("XaNMonD3fRoAAAAAAAAAASpDuc-aQyweenOg9IKT9QoXahg63wck3y_Qw1tMEhdi")
+                client = DropboxApi::Client.new(ENV['DROPBOX_TOKEN'])
                 client.create_folder("/#{lead.full_name}")
                 client.upload "/#{lead.full_name}/attached_file", binary_file
                 lead.file.destroy
