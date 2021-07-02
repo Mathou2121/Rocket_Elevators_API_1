8eb852c22c09d6bb68c269eb4fda058f89177c91
Roy Nouneh <transcript.jpg@gmail.com>
2021-07-02 12:42:27 -0400

    Zendesk could be ready but account problems

--- app/models/lead.rb
+++ app/models/lead.rb
@@ -1,5 +1,4 @@
 class Lead < ApplicationRecord   
-    ZendeskAPI::Ticket.create!(client, :subject => "Test Ticket", :comment => { :value => "This is a test" }, :submitter_id => client.current_user.id, :priority => "urgent") 
     has_one_attached :file
 end
 
